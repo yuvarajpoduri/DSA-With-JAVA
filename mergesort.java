5b674b07586d02f47fe18b56bcd6d7671afbacb6
Yuvaraj Poduri <118831011+yuvarajpoduri@users.noreply.github.com>
2024-07-25 12:59:58 +0530

    Delete mergesort.java

--- mergesort.java
+++
@@ -1,9 +0,0 @@
-package Sorting;
-import java.util.*;
-public class mergesort {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println(n);
-    }
-}
